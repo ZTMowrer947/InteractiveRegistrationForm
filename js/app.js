20bc5050982e53ffdda65237e62801edffe0d1ca
ZTMowrer947 <Zackmowrer@gmail.com>
2018-11-19 19:27:08 -0500

    Add function to run after page finishes loading

---
+++ js/app.js
@@ -0,0 +1,11 @@
+/*/////////////////////////////////////////////
+	Treehouse Full-Stack JavaScript Techdegree
+	Unit 3 Project: Interactive Form
+/////////////////////////////////////////////*/
+
+// Function to run when page finishes loading
+const onPageLoad = () => {
+}
+
+// Run onPageLoad function when page finishes loading
+$(onPageLoad);
