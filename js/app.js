0754891ff2d7c525277e18bb90bf4a9d234c0f1a
ZTMowrer947 <Zackmowrer@gmail.com>
2018-11-19 19:39:45 -0500

    Set job role field to be hidden when title select is not set to "other"

--- js/app.js
+++ js/app.js
@@ -5,8 +5,23 @@
 
 // Function to run when page finishes loading
 const onPageLoad = () => {
+	// Hide job role input field
+	$("input#other-title").hide();
+
 	// Set focus on username field
 	$("input#name").trigger("focus");
+
+	// Add change event listener for job title select
+	$("select#title").change(event => {
+		// If the new value is other,
+		if (event.target.value === "other") {
+			// Show the job role field
+			$("input#other-title").fadeIn();
+		} else { // Otherwise,
+			// Hide it
+			$("input#other-title").fadeOut();
+		}
+	});
 }
 
 // Run onPageLoad function when page finishes loading
